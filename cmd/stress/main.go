c497014e8eecd89694e0eac6e02d2d0c9f7dbca4
Asim Nepal <gnepal80m@gmail.com>
2025-11-18 17:42:25 -0700

    stress test for block info fetch

---
+++ cmd/stress/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/nepal80m/samurai/internal/dataset"
+)
+
+func main() {
+	numBlocks := flag.Int("n", 100, "Number of blocks to fetch")
+	flag.Parse()
+	startBlock := 18_908_895
+	endBlock := startBlock + *numBlocks - 1
+	r := dataset.NewDatasetReader(dataset.DATASET_DIR, dataset.SEGMENT_SIZE)
+	start := time.Now()
+	for n := startBlock; n <= endBlock; n++ {
+		_, err := r.GetBlock(uint32(n))
+		if err != nil {
+			panic(fmt.Errorf("failed to get block %d from dataset: %w", n, err))
+		}
+		// fmt.Println("Block", n, "with", len(entries), "accounts")
+	}
+	fmt.Println("Time taken to fetch", *numBlocks, "blocks:", time.Since(start))
+	r.Close()
+}
